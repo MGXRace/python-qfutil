277099dc410ea6b92771c21fc6d5d0537e11d03a
Alex Hartsell <kalhartt@gmail.com>
2015-05-01 00:34:33 -0400

    ReadTheDocs fix

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 import os
 
+rtd = os.environ.get('READTHEDOCS', False)
+
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -13,10 +15,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinxcontrib.napoleon'
 ]
-if os.getenv('SPELLCHECK'):
-    extensions += 'sphinxcontrib.spelling',
-    spelling_show_suggestions = True
-    spelling_lang = 'en_US'
 
 source_suffix = '.rst'
 master_doc = 'index'
@@ -26,19 +24,20 @@ author = 'kalhartt'
 copyright = '{0}, {1}'.format(year, author)
 version = release = '0.1.0'
 
-import sphinx_py3doc_enhanced_theme
-html_theme = "sphinx_py3doc_enhanced_theme"
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+if not rtd:
+    import sphinx_py3doc_enhanced_theme
+    html_theme = "sphinx_py3doc_enhanced_theme"
+    html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 
-pygments_style = 'trac'
-templates_path = ['.']
-html_use_smartypants = True
-html_last_updated_fmt = '%b %d, %Y'
-html_split_index = True
-html_sidebars = {
-    '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
-}
-html_short_title = '%s-%s' % (project, version)
-html_theme_options = {
-    'githuburl': 'https://github.com/MGXRace/python-qfutil/'
-}
+    pygments_style = 'trac'
+    templates_path = ['.']
+    html_use_smartypants = True
+    html_last_updated_fmt = '%b %d, %Y'
+    html_split_index = True
+    html_sidebars = {
+        '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
+    }
+    html_short_title = '%s-%s' % (project, version)
+    html_theme_options = {
+        'githuburl': 'https://github.com/MGXRace/python-qfutil/'
+    }
